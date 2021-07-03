567243e3cd576c37f767975b010554d287af7e0c
Just Me <just806me@gmail.com>
2021-07-03 18:35:47 +0300

    add firebase cli config

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 firebase.initializeApp({
   apiKey: "AIzaSyD1Yk18JOg2vAqv2_R1TxChcZkKgo12jpY",
@@ -12,5 +13,6 @@ firebase.initializeApp({
 })
 
 export const auth = firebase.auth()
-
 export const firestore = firebase.firestore()
+export const storage = firebase.storage()
+export default firebase

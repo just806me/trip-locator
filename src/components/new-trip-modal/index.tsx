55ccd284f0d911b0d14833d1785f39b860b11085
Just Me <just806me@gmail.com>
2021-07-03 19:47:57 +0300

    fix image upload

--- src/components/new-trip-modal/index.tsx
+++ src/components/new-trip-modal/index.tsx
@@ -1,18 +1,32 @@
 import { Button, Modal, Form, Input, Upload } from 'antd'
 import { PlusCircleOutlined, UploadOutlined } from '@ant-design/icons'
 import { useState } from 'react'
-import { RcFile } from 'antd/lib/upload'
+import { UploadRequestOption } from 'rc-upload/lib/interface'
 import { storage } from '../../firebase-config'
+import firebase from 'firebase'
 
-const handleUpload: (file: RcFile) => Promise<string> = async (file) => {
-  const fileRef = storage.ref().child(file.name)
-  await fileRef.put(file)
-  return await fileRef.getDownloadURL()
+const handleUpload = (addRef: (ref: firebase.storage.Reference) => void, options: UploadRequestOption) => {
+  if (!(options.file instanceof Blob)) {
+    console.error('received unexpected file', options.file)
+    return
+  }
+
+  const fileRef = storage.ref().child(`${new Date().getTime()}-${options.filename}`)
+
+  fileRef.put(options.file).on('state_changed',
+    snapshot => options.onProgress?.({ percent: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 } as any),
+    error => options.onError?.(error),
+    () => {
+      options.onSuccess?.(undefined, undefined!)
+      addRef(fileRef)
+    }
+  )
 }
 
 const NewTripModal = () => {
   const [visible, setVisible] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [images, setImages] = useState<firebase.storage.Reference[]>([])
   const [form] = Form.useForm()
 
   const submit = () => {
@@ -46,12 +60,16 @@ const NewTripModal = () => {
           </Form.Item>
 
           <Form.Item
-            name='images'
-            label='Upload images'
+            label='Images'
             valuePropName='fileList'
             getValueFromEvent={(e: any) => Array.isArray(e) ? e : e && e.fileList}
+            rules={[{ required: true }]}
           >
-            <Upload multiple name='images' listType='picture' action={handleUpload} >
+            <Upload
+              multiple
+              listType='picture'
+              customRequest={options => handleUpload(ref => setImages([...images, ref]), options)}
+            >
               <Button icon={<UploadOutlined />}>Click to upload</Button>
             </Upload>
           </Form.Item>

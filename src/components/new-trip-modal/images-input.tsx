52f288891f31775eea2a8b1977ec342b1db5213f
Just Me <just806me@gmail.com>
2021-07-03 20:13:43 +0300

    extract images input as seperate component

---
+++ src/components/new-trip-modal/images-input.tsx
@@ -0,0 +1,49 @@
+import { Rule } from 'antd/lib/form'
+import { Button, Form, Upload } from 'antd'
+import { UploadOutlined } from '@ant-design/icons'
+import { UploadRequestOption } from 'rc-upload/lib/interface'
+import firebase from 'firebase'
+import { storage } from '../../firebase-config'
+
+const handleUpload = (callback: (ref: firebase.storage.Reference) => void, options: UploadRequestOption) => {
+  if (!(options.file instanceof Blob)) {
+    console.error('received unexpected file', options.file)
+    return
+  }
+
+  const fileRef = storage.ref().child(`${new Date().getTime()}-${options.filename}`)
+
+  fileRef.put(options.file).on('state_changed',
+    snapshot => options.onProgress?.({ percent: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 } as any),
+    error => options.onError?.(error),
+    () => {
+      options.onSuccess?.(undefined, undefined!)
+      callback(fileRef)
+    }
+  )
+}
+
+interface ImagesInputProps {
+  label: string
+  rules: Rule[]
+  onUpload: (ref: firebase.storage.Reference) => void
+}
+
+const ImagesInput = ({ label, rules, onUpload }: ImagesInputProps) => (
+  <Form.Item
+    label={label}
+    rules={rules}
+    valuePropName='fileList'
+    getValueFromEvent={(e: any) => Array.isArray(e) ? e : e && e.fileList}
+  >
+    <Upload
+      multiple
+      listType='picture'
+      customRequest={options => handleUpload(onUpload, options)}
+    >
+      <Button icon={<UploadOutlined />}>Click to upload</Button>
+    </Upload>
+  </Form.Item>
+)
+
+export default ImagesInput
--- src/components/new-trip-modal/index.tsx
+++ src/components/new-trip-modal/index.tsx
@@ -1,27 +1,8 @@
-import { Button, Modal, Form, Input, Upload } from 'antd'
-import { PlusCircleOutlined, UploadOutlined } from '@ant-design/icons'
+import { Button, Modal, Form, Input } from 'antd'
+import { PlusCircleOutlined } from '@ant-design/icons'
 import { useState } from 'react'
-import { UploadRequestOption } from 'rc-upload/lib/interface'
-import { storage } from '../../firebase-config'
 import firebase from 'firebase'
-
-const handleUpload = (addRef: (ref: firebase.storage.Reference) => void, options: UploadRequestOption) => {
-  if (!(options.file instanceof Blob)) {
-    console.error('received unexpected file', options.file)
-    return
-  }
-
-  const fileRef = storage.ref().child(`${new Date().getTime()}-${options.filename}`)
-
-  fileRef.put(options.file).on('state_changed',
-    snapshot => options.onProgress?.({ percent: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 } as any),
-    error => options.onError?.(error),
-    () => {
-      options.onSuccess?.(undefined, undefined!)
-      addRef(fileRef)
-    }
-  )
-}
+import ImagesInput from './images-input'
 
 const NewTripModal = () => {
   const [visible, setVisible] = useState(false)
@@ -59,20 +40,7 @@ const NewTripModal = () => {
             <Input.TextArea />
           </Form.Item>
 
-          <Form.Item
-            label='Images'
-            valuePropName='fileList'
-            getValueFromEvent={(e: any) => Array.isArray(e) ? e : e && e.fileList}
-            rules={[{ required: true }]}
-          >
-            <Upload
-              multiple
-              listType='picture'
-              customRequest={options => handleUpload(ref => setImages([...images, ref]), options)}
-            >
-              <Button icon={<UploadOutlined />}>Click to upload</Button>
-            </Upload>
-          </Form.Item>
+          <ImagesInput label='Images' rules={[{ required: true }]} onUpload={i => setImages([...images, i])} />
 
           <Form.Item>
             <Button type='primary' htmlType='submit' loading={loading}>
